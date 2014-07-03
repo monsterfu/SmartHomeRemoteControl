09276d377acf5358b9352b3fb4a621dbba2cd06b
monsterfu <seanh@seanHtekiMac-mini.local>
2014-07-03 17:31:33 +0800

    Initial Commit

---
+++ SmartHomeRemoteControl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SmartHomeRemoteControl
+//
+//  Created by Monster on 14-7-3.
+//  Copyright (c) 2014年 Monster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
